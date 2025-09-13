4ae15b606214555d5db0fd2f4b312644fa65f00a
Md Aquib Anwer <aquibanwer001@gmail.com>
2025-09-14 00:41:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = process.env.REACT_APP_API_URL;
+//export const server = process.env.REACT_APP_API_URL;
 
-//export const server='http://localhost:5000';
+export const server='https://courseforge-server-1.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

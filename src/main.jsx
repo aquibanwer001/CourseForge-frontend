0a06d2e9658c3f1725ade443364d71fb47df1c98
Md Aquib Anwer <aquibanwer001@gmail.com>
2025-09-14 00:09:52 +0530

    Update server URL for deployed backend

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server='http://localhost:5000';
+export const server = process.env.REACT_APP_API_URL;
+
+//export const server='http://localhost:5000';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
